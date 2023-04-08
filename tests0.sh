c29bfb4a03928df15e8053162abf9b21586385c1
ng_tri <ngtri1410@gmail.com>
2023-04-08 08:38:02 +0700

    myTest/CI-CD

---
+++ tests0.sh
@@ -0,0 +1,8 @@
+python3 tests0.py
+python3 -c "from tests0 import test_prime; test_prime(1,False)"
+python3 -c "from tests0 import test_prime; test_prime(2,True)"
+python3 -c "from tests0 import test_prime; test_prime(8,False)"
+python3 -c "from tests0 import test_prime; test_prime(11,True)"
+python3 -c "from tests0 import test_prime; test_prime(25,False)"
+python3 -c "from tests0 import test_prime; test_prime(28,False)"
+
